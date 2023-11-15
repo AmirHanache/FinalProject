46e3cb13e0b9f840e526dc53237babf3dd5749cb
Chad Curtis <chad.curtis@nsc.edu>
2023-11-15 13:26:10 -0800

    Optimized the code, also fixed error when it reaches last image.

--- summer/movingscreen.py
+++ summer/movingscreen.py
@@ -2,11 +2,12 @@ import sys
 sys.path.insert(0, '..')
 from graphics import *
 
-win = GraphWin('Moving Scene', 600, 600)
-
+win = GraphWin('Moving Scene', 600, 600, autoflush=False)
+im = Image(Point(300, 300), 'Summer_0.gif')
 i = 0
 while True:
-
+    im.undraw()
+    i = i % 1011
     im = Image(Point(300, 300), 'Summer_{}.gif'.format(i))
     im.draw(win)
     update(20)
