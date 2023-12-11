d3e264c57da4cac3a45ed05617487f22b2f31ff3
Amanda Doonan <a.doonan91@gmail.com>
2023-12-10 18:17:53 -0800

    121023

--- matching.py
+++ matching.py
@@ -20,12 +20,13 @@ class MemoryGame:
         self.drawBoard()
 
     def createCards(self):
-        tricks = list("SIT", "STAY", "PAW", "TOUCH", "DOWN", "ROLL")*2
+
+        tricks = ["SIT", "STAY", "PAW", "TOUCH", "DOWN", "ROLL", "SIT", "STAY", "PAW", "TOUCH", "DOWN", "ROLL"]
         random.shuffle(tricks)
         for i in range(3):
             row = []
-            for j in range(3):
-                card = Card(values.pop(), (i,j))
+            for j in range(4):
+                card = Card(tricks[i*4 + j], (i,j))
                 row.append(card)
             self.cards.append(row)
 
@@ -35,11 +36,14 @@ class MemoryGame:
 
         for i in range(3):
             row = []
-            for j in range(3):
+            for j in range(4):
                 rect = Rectangle(Point(j * (cardSize + margin), i * (cardSize + margin)),
                                  Point((j + 1) * cardSize + j * margin, (i + 1) * cardSize + i * margin))
                 rect.draw(self.win)
+                rect.setFill("lightgreen")
                 label = Text(rect.getCenter(), "")
+                label.setStyle("bold")
+                label.draw(self.win)
                 label.setSize(12)
                 row.append((rect, label))
             self.board.append(row)
@@ -50,51 +54,52 @@ class MemoryGame:
 
         if not card.isFlipped:
             label.setText(str(card.value))
+            rect.setFill("lightpink")
         else:
             label.setText("")
+            rect.setFill("lightgreen")
         card.isFlipped = not card.isFlipped
+
 
     def clicks(self, click):
         for i in range(3):
-            for j in range(3):
-                rect, _ = self.board[i][j]
-                if rect.getP1().getX() <= click.getrX() <= rect.getP2().getX() and \
+            for j in range(4):
+                rect,label = self.board[i][j]
+                if rect.getP1().getX() <= click.getX() <= rect.getP2().getX() and \
                         rect.getP1().getY() <= click.getY() <= rect.getP2().getY():
                     card = self.cards[i][j]
-                    if card.position not in self.selectCards:
-                        self.flip_card(card)
-                        self.selectCards.append(card.position)
-                        return
+                    self.flipCard(card)
+                    print(card.value)
+                    return card
 
     def playGame(self):
         while True:
-            click = self.qin.getMouse()
-            self.clicks(click)
+            click = self.win.getMouse()
+            card1 = self.clicks(click)
+            click = self.win.getMouse()
+            card2 = self.clicks(click)
 
-            if len(self.selectCards) == 2:
+            if card1.value == card2.value:
+                print("You got it!")
+                self.selectCards.append(card1.position)
+                self.selectCards.append(card2.position)
+                if all(card.isFlipped for row in self.cards for card in row):
+                    print("Great training!")
+                    self.win.close()
+                    return
+            else:
+                print("Try again!")
                 time.sleep(1)
-                pos1, pos2 = self.selectCards
-                card1, card2 = self.cards[pos1[0]][pos1[1]], self.cards[pos2[0]][pos2[1]]
-
-                if card1.value == card2.value:
-                    for pos in self.selectCards:
-                        self.cards[pos[0]][pos[1]].isFlipped = True
-                    if all(card.isFlipped for row in self.cards for card in row):
-                        print("Great training!")
-                        self.win.close()
-                        return
-
-                    else:
-                        for pos in self.selectCards:
-                            self.flipCard(self.cards[pos[0]][pos[1]])
-                    self.selectCards = []
+                self.flipCard(card1)
+                self.flipCard(card2)
 
 def main():
-    win = GraphWin("Training Game", 400, 400)
+    win = GraphWin("Training Game", 430, 430)
     win.setBackground("lightblue")
 
+
     game = MemoryGame(win)
-    game.play()
+    game.playGame()
 
 
 if __name__ == "__main__":
